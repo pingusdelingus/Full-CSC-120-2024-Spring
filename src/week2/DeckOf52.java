99dc910b1d3e07dc1dc51693372b7f21aa31fd28
pingusdelingus <alphaspammail2016@gmail.com>
2024-02-28 23:45:57 -0500

    created 2d array for cards with 4 rows and 13 columns. 4 suits and 13 cards in each suit. run 2 nested for loops in order to initialize variables in each suit. need to title each row with suit names. need to create shuffle function to shuffle 2d array(math.random class) eventually turn into poker game in the terminal :p

---
+++ src/week2/DeckOf52.java
@@ -0,0 +1,21 @@
+package week2;
+
+public class DeckOf52 {
+    private int[][] deck = new int[4][13];
+    private int currentCard;
+    private int index;
+    private int jdex;
+
+    public DeckOf52() {
+        for (index = 0; index < deck.length; index++ ){
+            for (jdex = 0; jdex < deck[index].length; jdex++){
+
+
+            }// end of inner for loop
+        }// end of outer for loop
+
+
+    }// end of shuffle
+
+
+}// end of DeskOf52 CLASS (METHOD SETUP NO MAIN)
