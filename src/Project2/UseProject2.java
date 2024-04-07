b9689ca055b124f4bb0a51bce18f159e958adeaa
pingusdelingus <alphaspammail2016@gmail.com>
2024-04-07 00:07:31 -0400

    Testing file reading 4/6/2024

--- src/Project2/UseProject2.java
+++ src/Project2/UseProject2.java
@@ -1,59 +1,91 @@
 package Project2;
+
 import java.util.Locale;
 import java.util.Scanner;
+import java.io.*;
+
 
 public class UseProject2 {
     private static final Scanner keyboard = new Scanner(System.in);
 
-    private static void main(String[] args){
+    private static void main(String[] args) {
+
+        String fileName;
+        FileReader fromReader = null;
+        int aChar;
+
+
+
         System.out.println("Welcome to the Forestry Simulation");
         System.out.println("----------------------------------");
 
-        char userInput;
+        // Tries to open file from CMD line arguments at position 0
+        // if found, prints out whole file.
 
+        try{
+            fromReader = new FileReader(args[0]);
+            aChar = fromReader.read();
+            while(aChar != -1){
+                System.out.print((char) aChar);
+                aChar = fromReader.read();
 
-        do{
-            try{
-                userInput = keyboard.next
             }
+        } catch(FileNotFoundException e){
+            System.out.println("404: File not found" + e.getMessage());
+
+        }catch(IOException e){
+            System.out.println("Something went wrong! " + e.getMessage());
+        }finally {
+            if (fromReader != null){
+                try {
+                    fromReader.close();
+                }catch(IOException e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
 
 
-        } while()
 
 
-    }// end of main METHOD
 
-    private static void printForest(Forest currentForest){
 
-    }// end of printForest METHOD
 
-    private static void addTree(currentForest){
 
-    }// end of addTree METHOD
 
-    private static boolean simulateGrowth(Forest currentForest){
+}// end of main METHOD
 
-    }// end of simulateGrowth METHOD
+private static void printForest(Forest currentForest) {
 
-    private static boolean cutTree(Forest currentForest){
+}// end of printForest METHOD
 
-    }// end of cutTree METHOD
+private static void addTree(currentForest) {
 
-    private static reapTrees(Forest currentForrest){
+}// end of addTree METHOD
 
-    }// end of reapTrees METHOD
+private static boolean simulateGrowth(Forest currentForest) {
 
-    private static saveForestAsDB(Forest currentForest){
+}// end of simulateGrowth METHOD
 
-    }// end saveForestAsDB
+private static boolean cutTree(Forest currentForest) {
 
-    private static void loadForest(){
+}// end of cutTree METHOD
 
-    }// end of loadForest METHOD
+private static reapTrees(Forest currentForrest) {
 
-    private static void nextForest(){
+}// end of reapTrees METHOD
 
-    }// end of nextForest
+private static saveForestAsDB(Forest currentForest) {
+
+}// end saveForestAsDB
+
+private static void loadForest() {
+
+}// end of loadForest METHOD
+
+private static void nextForest() {
+
+}// end of nextForest
 
 
 
